bd5606615881e475860390b7ce9e8537651a280f
Jake Jongejans <jake.jongejans@voiceworks.com>
2022-06-21 12:07:01 +0200

    feat(group-item): Adds group item component with examples.

--- MRC/src/App.tsx
+++ MRC/src/App.tsx
@@ -1,19 +1,22 @@
-import { Component, createSignal } from 'solid-js';
-import './App.scss';
-import Counter from './Counter';
+import { Component, createSignal } from 'solid-js'
+import './App.scss'
+import './colours.scss'
+import GroupItem from './components'
 
 const App: Component = () => {
-  const [counter, setCounter] = createSignal(0);
-  setInterval(setCounter, 1000, (c: number) => c + 6);
+  const [counter, setCounter] = createSignal(0)
+  setInterval(setCounter, 1000, (c: number) => c + 6)
 
   return (
     <>
       <div>
         <h1 class="header">{counter()}</h1>
       </div>
-      <Counter />
+      <GroupItem name="Group A" lastChat="Scott: I love this shit" status="green" />
+      <GroupItem name="Group B" lastChat="Merijn: Love LoRa" status="yellow" />
+      <GroupItem name="Group C" lastChat="Merijn: Wi-Fi is makkelijker" status="yellow" />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ MRC/src/components/group-item/index.tsx
@@ -0,0 +1,16 @@
+import './GroupItem.scss'
+import cl from 'clsx'
+
+export default function GroupItem(props) {
+  return (
+    <div class='group-item'>
+      <div class='group-item__image'>
+        <div class={cl(`group-item__indicator group-item__indicator--${props.status}`)}></div>
+      </div>
+      <div class='group-item__text'>
+        <div class='group-item__name'>{props.name}</div>
+        <div class='group-item__last-chat'>{props.lastChat}</div>
+      </div>
+    </div>
+  )
+}
