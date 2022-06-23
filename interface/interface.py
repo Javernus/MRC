34b7d1ccf77307a05af73e39e2e039cf24536eee
Merijn Laks <merijn.laks@student.uva.nl>
2022-06-23 12:54:24 +0200

    able to send data

--- interface/interface.py
+++ interface/interface.py
@@ -8,26 +8,20 @@ interface = meshtastic.serial_interface.SerialInterface()
 
 
 def onReceive(packet, interface):  # called when a packet arrives
-    print(f"Received: {packet}")
-    # de-serialize data and
-
-
-def onConnection(interface, topic=pub.AUTO_TOPIC):  # called when we (re)connect to the radio
-    # defaults to broadcast, specify a destination ID if you wish
-    interface.sendText("Connected to mesh network")
+    print(packet)
 
 
 pub.subscribe(onReceive, "meshtastic.receive")
-pub.subscribe(onConnection, "meshtastic.connection.established")
 
-interface.sendText("Testing script")
+# interface.sendText("Testing script")
 
 # event loop, listen for input on stdin and send data
 while True:
     data = sys.stdin.readline()
     if data:
+        # interface.sendText(data)
         interface.sendData(
-            data,
+            bytes(data.encode('utf-8')),
             destinationId='^all',
             portNum=256,
             wantAck=False,
