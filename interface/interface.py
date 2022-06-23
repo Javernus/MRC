248bce953571b12e29228c7d3c3ab3f9f7072c33
Merijn Laks <merijn.laks@student.uva.nl>
2022-06-23 11:42:06 +0200

    started working on node interface

--- interface/interface.py
+++ interface/interface.py
@@ -1,1 +1,18 @@
 import meshtastic
+import meshtastic.serial_interface
+from pubsub import pub
+
+# By default will try to find a meshtastic device, otherwise provide a device path like /dev/ttyUSB0
+interface = meshtastic.serial_interface.SerialInterface()
+
+def onReceive(packet, interface): # called when a packet arrives
+    print(f"Received: {packet}")
+
+def onConnection(interface, topic=pub.AUTO_TOPIC): # called when we (re)connect to the radio
+    # defaults to broadcast, specify a destination ID if you wish
+    interface.sendText("hello mesh")
+
+pub.subscribe(onReceive, "meshtastic.receive")
+pub.subscribe(onConnection, "meshtastic.connection.established")
+
+interface.sendText("Testing script")
