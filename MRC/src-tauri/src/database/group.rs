d0c4b7f52e6bb30c3a8cb29512483427b8e8cea5
Ilya Claassen <i.c.educational@protonmail.com>
2022-06-27 16:30:28 +0200

    feat(database): Increases maximum of unique group id.

--- MRC/src-tauri/src/database/group.rs
+++ MRC/src-tauri/src/database/group.rs
@@ -19,7 +19,7 @@ impl Group {
     /// returns: Group
     pub fn new(name: &str, bio: &str) -> Group {
         let alphabet: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-        let id: i32 = nanoid!(3, &alphabet).parse().unwrap();
+        let id: i32 = nanoid!(9, &alphabet).parse().unwrap();
 
         Group {
             id,
