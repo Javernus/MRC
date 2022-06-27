d877186f247d5eb3821540e6ac9156e339d80708
Merijn Laks <merijn.laks@student.uva.nl>
2022-06-27 15:06:29 +0200

    started working on receiver.rs for front-end

---
+++ MRC/src-tauri/src/receiver.rs
@@ -0,0 +1,31 @@
+use std::os::unix::net::UnixStream;
+use std::io::prelude::*;
+
+use std::{thread, time};
+
+
+fn start_receiver() -> () {
+    // connect to socket
+    let mut stream = match UnixStream::connect("ipc.sock") {
+        Ok(stream) => stream,
+        Err(e) => {
+            println!("Couldn't connect: {:?}", e);
+            return Ok(());
+        }
+    };
+
+    loop {
+        let mut message_buffer = [0; 256];
+        stream.read(&mut message_buffer).expect("todo");
+        let mut message = String::from_utf8(Vec::from(message_buffer)).expect("todo");
+
+        if !message.is_empty() {
+            // emit the `event-name` event to all webview windows on the frontend
+            app.emit_all(
+                "message_event",
+                Payload { message: message.into() }
+            ).unwrap();
+            // println!("{message}")
+        }
+    }
+}
