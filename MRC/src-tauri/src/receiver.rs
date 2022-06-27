590dab5dea71044164b1a0e160278cf7d65a14b7
Jake Jongejans <jake.jongejans@voiceworks.com>
2022-06-27 16:09:57 +0200

    feat(add message)

--- MRC/src-tauri/src/receiver.rs
+++ MRC/src-tauri/src/receiver.rs
@@ -7,7 +7,9 @@ use std::{thread, time};
 use tauri::{Manager, Window};
 
 use crate::database::chat::Chat;
-use crate::database::group::Group;
+use crate::database::chat;
+use crate::database;
+
 
 
 // the payload type must implement `Serialize` and `Clone`.
@@ -18,35 +20,35 @@ struct Payload {
 
 pub fn start_receiver(window: Window) -> () {
     // connect to socket
-    let mut stream = match UnixStream::connect("/tmp/ipc.sock") {
-        Ok(stream) => stream,
-        Err(e) => {
-            println!("Couldn't connect: {:?}", e);
-            return;
-        }
-    };
+    // let mut stream = match UnixStream::connect("/tmp/ipc.sock") {
+    //     Ok(stream) => stream,
+    //     Err(e) => {
+    //         println!("Couldn't connect: {:?}", e);
+    //         return;
+    //     }
+    // };
 
     loop {
-        thread::sleep(time::Duration::from_secs(2));
+        thread::sleep(time::Duration::from_secs(5));
         println!("Waiting for message...");
         // window.emit("custom_event", Payload {
         //     message: "Hello from Rust!".to_string(),
         // });
-        let mut message_buffer = [0; 256];
-        stream.read(&mut message_buffer).expect("todo");
-        let mut message = String::from_utf8(Vec::from(message_buffer)).expect("todo");
+        // let mut message_buffer = [0; 256];
+        // stream.read(&mut message_buffer).expect("todo");
+        // let mut message = String::from_utf8(Vec::from(message_buffer)).expect("todo");
+        let message = "Hello from Rust!".to_string();
 
         if !message.is_empty() {
         //     // emit the `event-name` event to all webview windows on the frontend
             window.emit(
                 "refetch_chat",
-                Payload {message: message.to_string()}
+                Payload { message: message.to_string() }
             ).unwrap();
 
-            let chat: Chat = Chat::new(group_id, 123456789012, "Name", message);
+            let chat: Chat = Chat::new(8, 123456789012, "Name", &message);
 
-            database::save_chat(chat)
-        //     // println!("{message}")
+            database::save_chat(&chat)
         }
     }
 }
