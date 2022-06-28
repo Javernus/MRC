baafbf73875b7f8ae17112a34b65e7d43d6a4466
Scott Scherpenzeel <scott.scherpenzeel@student.uva.nl>
2022-06-28 13:31:38 +0200

    data encoding with group password

---
+++ MRC/src/groupencoding/groupencoding.rs
@@ -0,0 +1,45 @@
+#[macro_use] extern crate magic_crypt;
+extern crate base64;
+extern crate regex;
+use magic_crypt::MagicCryptTrait;
+use regex::Regex;
+
+fn encode(name:&str, password:&str, data:&str) -> String {
+
+    println!("{}",name);
+    println!("{}",password);
+    println!("{}",data);
+
+    let datatoencode = format!("{},{}", name, data);
+    let mcrypt = new_magic_crypt!(password, 256);
+    let encrypted_string = mcrypt.encrypt_str_to_base64(datatoencode);
+    println!("{}", encrypted_string);
+    return encrypted_string;
+    // println!("{:#?}",encoded)
+    // let decrypted_string = mcrypt.decrypt_base64_to_string(&encrypted_string).unwrap();
+    // println!("{}",decrypted_string);
+}
+
+fn decode(password:&str, data:String) -> (bool, String, String){
+
+    let mcrypt = new_magic_crypt!(password, 256);
+    let decrypted_string = mcrypt.decrypt_base64_to_string(&data);
+    match decrypted_string {
+        Ok(data) => {
+            let re = Regex::new("^([^,]*),(.*)").unwrap();
+            let name = re.captures(&data).unwrap();
+            let datatosend = (true, name[1].to_string(), name[2].to_string());
+            println!("{:#?}", datatosend);
+            return datatosend;
+        },
+        Err(msg) => {
+            return(false, "".to_string(), "".to_string())
+        }
+    }
+}
+
+fn main() {
+
+    let encodeddata:String = encode("test","test","test");
+    decode("test", encodeddata);
+}
