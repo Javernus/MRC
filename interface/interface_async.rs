2ba0f27e0616f53668cc6871bad313f9e00f4956
Merijn Laks <merijn.laks@student.uva.nl>
2022-06-29 02:18:42 +0200

    Remove old files

--- interface/interface_async.rs
+++ interface/interface_async.rs
@@ -1,6 +1,4 @@
 extern crate core;
-use std::os::unix::net::UnixStream;
-use std::io::prelude::*;
 // use tauri::Window;
 // use crate::database::chat::Chat;
 // use crate::database;
@@ -10,6 +8,15 @@ use queues::*;
 extern crate global;
 use global::Global;
 
+use tokio::io::Interest;
+use tokio::net::UnixStream;
+use std::error::Error;
+use std::io;
+use std::str;
+use std::time::Duration;
+use tokio::time::sleep;
+
+
 // the payload type must implement `Serialize` and `Clone`.
 // #[derive(Clone, serde::Serialize)]
 // struct Payload {
@@ -17,80 +24,72 @@ use global::Global;
 // }
 
 // TODO implement custom structs
-static INCOMING_QUEUE: Global<Queue<String>> = Global::new();
 static OUTGOING_QUEUE: Global<Queue<String>> = Global::new();
 
-
-fn read_from_socket(mut stream: &UnixStream) -> () {
-    println!("trying to read from socket..");
-    let mut buffer = [0;255];
-    let _ = &stream.read(&mut buffer).expect("read from socket");
-
-    let incoming_message = String::from_utf8(Vec::from(buffer)).expect("converting");
-
-    // TODO do this with a delimeter
-    if !incoming_message.is_empty() {
-        println!("read {incoming_message} from socket");
-        (*INCOMING_QUEUE.lock_mut().unwrap()).add(incoming_message).expect("adding to queue");
-    }
-}
-
-// convert to send_message function
-fn write_to_socket(mut stream: &UnixStream, message: String) -> (){
-    let mut buffer = message.as_bytes();
-    stream.write_all(&mut buffer).expect("writing to socket");
-
-    //TODO
-}
-
-fn send_message(message: String) -> (){
+fn send_message(message: String) -> () {
     (*OUTGOING_QUEUE.lock_mut().unwrap()).add(message).expect("adding message");
 }
 
 // TODO make this its own function
 #[tokio::main]
-async fn main() -> () {
-    *INCOMING_QUEUE.lock_mut().unwrap() = queue![];
+async fn main() -> Result<(), Box<dyn Error>> {
     *OUTGOING_QUEUE.lock_mut().unwrap() = queue![];
 
-    //connect to socket
-    let stream = match UnixStream::connect("/tmp/ipc.sock") {
-        Ok(stream) => Ok(stream),
-        Err(e) => {
-            println!("Couldn't connect: {:?}", e);
-            Err(e)
-        }
-    };
+    let stream = UnixStream::connect("/tmp/ipc.sock").await?;
 
     loop {
-        // check for incoming messages in socket
-        read_from_socket(stream.as_ref().unwrap());
+        let ready = stream.ready(Interest::READABLE | Interest::WRITABLE).await?;
 
         if rand::random() {
-            println!("sending test message..");
-            send_message("testMessage".parse().unwrap())
+            send_message("Hello world".parse().unwrap());
         }
 
-        // signal tauri if we have messages
-        if (*INCOMING_QUEUE.lock_mut().unwrap()).size() > 0 {
-            let incoming = (*INCOMING_QUEUE.lock_mut().unwrap()).remove().unwrap();
-            println!("we have an incoming message: {incoming}");
+        if ready.is_readable() {
+            let _ = sleep(Duration::from_millis(1000)).await;
+            let mut data = vec![0; 255];
 
-            // let chat: Chat = Chat::new(8, 123456789012, "Name", &incoming);
-            // database::save_chat(&chat);
-            //
-            // window.emit(
-            //     "refetch_chat",
-            //     Payload { message: incoming.to_string() }
-            // ).unwrap();
+            match stream.try_read(&mut data) {
+                Ok(_) => {}
+                Err(ref e)  if e.kind() == io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                Err(e) => {
+                    return Err(e.into());
+                }
+            };
+
+            let incoming_message = str::from_utf8(&data).unwrap();
+
+            if !incoming_message.is_empty() {
+                println!("read {incoming_message} from socket");
+
+                // let chat: Chat = Chat::new(8, 123456789012, "Name", &incoming);
+                // database::save_chat(&chat);
+                //
+                // window.emit(
+                //     "refetch_chat",
+                //     Payload { message: incoming.to_string() }
+                // ).unwrap();
+            }
         }
 
-        // check for outgoing messages in queue
-        if (*OUTGOING_QUEUE.lock_mut().unwrap()).size() > 0 {
-            println!("we have an outgoing message");
-            let outgoing = (*OUTGOING_QUEUE.lock_mut().unwrap()).remove().unwrap();
-            write_to_socket( stream.as_ref().unwrap(), outgoing);
-        }
+        if ready.is_writable() {
+            let _ = sleep(Duration::from_millis(1000)).await;
+            if (*OUTGOING_QUEUE.lock_mut().unwrap()).size() > 0 {
+                let outgoing_message = (*OUTGOING_QUEUE.lock_mut().unwrap()).remove().unwrap();
 
+
+                match stream.try_write((outgoing_message + "\n").as_bytes()) {
+                    Ok(_) => {}
+                    Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                        continue;
+                    }
+                    Err(e) => {
+                        return Err(e.into());
+                    }
+                }
+            }
+
+        }
     }
 }
