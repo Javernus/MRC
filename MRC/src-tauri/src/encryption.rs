fab62057e7b86d25ea918f4ac6e8aa34941bcfd7
Ilya Claassen <i.c.educational@protonmail.com>
2022-06-29 20:03:51 +0200

    feat(tests): Adds tests to encryption and hashing.
    Bug: hash salt keeps changing.

--- MRC/src-tauri/src/encryption.rs
+++ MRC/src-tauri/src/encryption.rs
@@ -1,14 +1,37 @@
 use magic_crypt::{new_magic_crypt, MagicCryptTrait, MagicCrypt256};
 
+/// Returns key-value of password.
+///
+/// # Arguments
+///
+/// * `password`: password to generate key for.
+///
+/// returns: MagicCrypt256
 fn generate_key(password: &str) -> MagicCrypt256 {
   new_magic_crypt!(password, 256)
 }
 
+/// Encrypts a string using the given password.
+///
+/// # Arguments
+///
+/// * `text`: string to encrypt.
+/// * `password`: password to use for encryption.
+///
+/// returns: String
 pub fn encrypt(text: &str, password: &str) -> String {
     let key = generate_key(password);
     key.encrypt_str_to_base64(text)
 }
 
+/// Decrypts a string using the given password.
+///
+/// # Arguments
+///
+/// * `ciphertext`: string to decrypt.
+/// * `password`: password to use for decryption.
+///
+/// returns: String
 pub fn decrypt(ciphertext: &str, password: &str) -> String {
     let key = generate_key(password);
     match key.decrypt_base64_to_string(ciphertext) {
--- MRC/src-tauri/src/hashing.rs
+++ MRC/src-tauri/src/hashing.rs
@@ -1,8 +1,16 @@
 use argon2::password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString, rand_core::OsRng};
 use argon2::Argon2;
 
+/// Hashes the password.
+///
+/// # Arguments
+///
+/// * `password`: password to hash.
+///
+/// returns: String
 pub fn hash_password(password: &str) -> String {
   let salt: SaltString = SaltString::generate(&mut OsRng);
+  println!("{}", salt);
   let argon2: Argon2 = Argon2::default();
   match argon2.hash_password(password.as_ref(), &salt) {
     Ok(hash) => hash.to_string(),
@@ -14,11 +22,27 @@ pub fn hash_password(password: &str) -> String {
   }
 }
 
-pub fn verify_password(password: &str, password_hash: &str) -> bool {
+/// Verifies whether the password corresponds to the hashed password.
+///
+/// # Arguments
+///
+/// * `password`: password to verify (given by the user).
+/// * `hashed_password`: password to verify withe (retrieved from database).
+///
+/// returns: bool
+pub fn verify_password(password: &str, hashed_password: &str) -> bool {
   let argon2: Argon2 = Argon2::default();
-  let parsed_hash: PasswordHash = match PasswordHash::new(password_hash) {
+  let parsed_hash: PasswordHash = match PasswordHash::new(hashed_password) {
     Ok(hash) => hash,
     Err(_) => return false,
   };
   argon2.verify_password(password.as_ref(), &parsed_hash).is_ok()
+}
+
+#[test]
+fn test_hashing() {
+  let password: &str = "password123";
+  let hashed_password: String = hash_password(password);
+
+  assert!(verify_password(password, &hashed_password));
 }
